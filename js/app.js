a660925e2b0ca25197350fc85805b8cb18b77eeb
Tarek <tarekipad@gmail.com>
2020-10-20 12:46:57 +0200

    finished destktop active state

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let shareButton = document.querySelector('#shareBtn');
+let sharePopUp = document.querySelector('.article__right__active__state');
+
+shareButton.addEventListener('click', function () {
+    sharePopUp.style.display = 'flex';
+});
+
+alert("Hello");
+
+
+console.log("Hello");
+
